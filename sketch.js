899db97201d4324ef5baf7411f7941e159751cfa
olliollioxinfree <faithmiller850@gmail.com>
2025-04-30 14:23:19 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(1080, 1500);
+  createCanvas(1080, 1080);
   imageMode (CENTER);
   rectMode (CENTER);
   textAlign (CENTER);
@@ -8,18 +8,18 @@ function setup() {
 
 function draw() {
   background(255, 0, 0);
-  image (splash, width/2, 100, 500, 300);
-  image (run, width/2, 500, 500, 300);
-  image (fork, width/2, 700, 500, 300);
-  image (cave, width/2, 900, 500, 300);
-  image (uwin, width/2, 1100, 500, 300);
+  image (splash, width/2, 100, 400, 200);
+  image (run, width/2, 300, 400, 200);
+  image (fork, width/2, 500, 400, 200);
+  image (cave, width/2, 700, 400, 200);
+  image (uwin, width/2, 900, 400, 200);
     fill ("yellow");
   
   
   textSize(25);
-  rect (width/2, 1400, 225, 50);
+  rect (width/2, 1050, 225, 50);
    fill(255,0,0);
-  text("YOU WIN!", width/2, 1400)
+  text("YOU WIN!", width/2, 1050)
   
   
   
